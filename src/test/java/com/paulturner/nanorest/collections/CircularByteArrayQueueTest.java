0ee1c79acd4e61e2da10f9dfd99708228c0ef7c9
pauljeremyturner <pauljeremyturner@yahoo.com>
2019-05-02 12:26:15 +0100

    NNR-4: minor refactor

---
+++ src/test/java/com/paulturner/nanorest/collections/CircularByteArrayQueueTest.java
@@ -0,0 +1,43 @@
+package com.paulturner.nanorest.collections;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CircularByteArrayQueueTest {
+    private CircularByteArrayQueue queue;
+
+    @Before
+    public void before() throws Exception {
+        queue = new CircularByteArrayQueue(80);
+    }
+
+    @Test
+    public void shouldIndicateDoesNotContainWhenEmpty() throws Exception {
+
+        assertThat(queue.contains("hello, is it me you're looking for?".getBytes(StandardCharsets.US_ASCII))).isFalse();
+    }
+
+    @Test
+    public void shouldIndicateContainsWhenContains() throws Exception {
+        IntStream.rangeClosed(48, 57).forEach(
+                i -> queue.offer((byte) i)
+        );
+
+        assertThat(queue.contains("0123456789".getBytes(StandardCharsets.US_ASCII))).isTrue();
+    }
+
+    @Test
+    public void shouldIndicateContainsWhenContainsAfterCircularOverflow() throws Exception {
+        for (byte b : " disorders]]</text><sha1>tswevljrxdlsg8zl3bs205kb6sec6jc</sha1> </revision></page".getBytes(StandardCharsets.US_ASCII)) {
+            queue.offer(b);
+        }
+
+        assertThat(queue.contains("</page".getBytes(StandardCharsets.US_ASCII))).isTrue();
+    }
+
+}
